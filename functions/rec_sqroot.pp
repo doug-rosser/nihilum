94ca021e6888c8a62dea7b0e85b4632fc186c09c
Doug Rosser <doug.rosser@puppet.com>
2017-10-23 13:37:28 -0700

    Add source of the algorithm

--- functions/rec_sqroot.pp
+++ functions/rec_sqroot.pp
@@ -1,3 +1,6 @@
+# Original non-recursive code from:
+# https://stackoverflow.com/questions/1623375/writing-your-own-square-root-function
+
 function nihilum::rec_sqroot(Integer $a, Integer $b, Integer $x) >> Integer {
   #notice("\$a = ${a} , \$b = ${b}")
   if ( abs($a - $b) > 100 ) {
